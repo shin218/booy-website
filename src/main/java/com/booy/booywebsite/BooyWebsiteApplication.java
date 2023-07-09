1d7c9dba42883e5df4fdccba144ff2b5d2b45623
shinyoung <maxjuny78@gmail.com>
2023-07-09 16:26:55 +0900

    index.html 완성

---
+++ src/main/java/com/booy/booywebsite/BooyWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.booy.booywebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooyWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooyWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/booy/booywebsite/BooyWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booy.booywebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooyWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
